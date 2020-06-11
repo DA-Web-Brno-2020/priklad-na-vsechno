c3689e14d3727efa30908cb83b7b519108f8f881
Luděk Roleček <ludek@rolecek.cz>
2020-06-11 16:51:57 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
     <router-view/>
+
   </div>
 </template>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+html { box-sizing: border-box; }
+*, ::before, ::after { box-sizing: inherit; }
+
+html {
+  --vue-green: #41b883;
+  --vue-dark: #35495e;
+}
+
+
+body {
+  margin: 2rem;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 18px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  color: var(--vue-green);
+}
+
+h1 {
+  color: var(--vue-dark);
+  font-size: 48px;
+  text-transform: uppercase;
+  padding-bottom: 10px;
+  border-bottom: 10px solid var(--vue-green);
+}
+
+h2 {
+  font-size: 28px;
+}
+
+h3 {
+  font-size: 22px;
+}
+
+
+a {
+  color: var(--vue-green);
+}
+
+a:hover {
+  color: var(--vue-dark);
+}
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <h1>Ultimátní příklad na všechno z Vue</h1>
+
+    <p>Zopakuješ si třeba:</p>
+
+    <ol class="menu">
+      <li>Jak na tránky v aplikaci pomocí Vue routeru</li>
+      <li>Zobrazování dat v šabloně a události</li>
+      <li>Skrývání/zobrazování prvků pomocí v-if a v-show</li>
+      <li>Nastavování atributů pomocí v-bind</li>
+      <li>Přidávání CSS tříd na prvky</li>
+      <li>Zobrazování dat z pole pomocí v-for</li>
+      <li>Vytváření komponent</li>
+      <li>Komunikace mezi komponentami pomocí props a custom events ($emit)</li>
+      <li>Získávání dat z formulářových prvků pomocí v-model</li>
+      <li>Computed property</li>
+      <li>Created - spuštení při zobrazení komponenty</li>
+    </ol>
+
+    <p>
+      <strong>Je toho hrozně moc!</strong> Snad to stihneme. Ale pravděpodobně ne.
+      Každopádně se zbytečně zdržuješ tím, že čteš tyhle blbosti. Šup do práce!
+    </p>
+
   </div>
 </template>
+
+<style>
+  .menu li {
+    margin-bottom: 0.5em;
+  }
+</style>
